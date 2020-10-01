9bcad871d8c6eb8c9a9d0b9b2cb0b1de62ad8a92
ilhan_koral <ilhan.koral@ttl.be>
2020-10-01 04:01:35 +0200

    templates loop

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,22 @@
 from flask import render_template
 from app import app
 
+
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Spiderman'}
+
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+
     # return render_template('index.html',title = 'Home', templateuser = user)
-    return render_template('index.html', templateuser = user)
+    return render_template('index.html', templateuser=user, posts = posts)
