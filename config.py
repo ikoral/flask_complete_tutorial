c51a2eb722e176db6635f78e8311c705407314c1
ilhan_koral <ilhan.koral@ttl.be>
2020-10-04 13:17:51 +0200

    database and migration created

--- config.py
+++ config.py
@@ -1,6 +1,16 @@
 # Normally  we put config file to .gitignore and don't share
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    
+
+
 
